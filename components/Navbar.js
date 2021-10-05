ecc5968bde1bd01c00632c6fc0668aa04d459d00
Chris <chris.ware.dev@gmail.com>
2021-10-04 23:05:24 -0700

    Font Update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,46 +1,43 @@
-import Link from 'next/link'
-import styles from './Navbar.module.css'
-
+import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.nav}>
-          <div className={styles.menu}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div className={styles.horiMenu}>
-            <ul>
-              <li>
-                <Link href='#buy'>
-                  <a>Buy</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#blog'>
-                  <a>Blog</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='#help'>
-                  <a>Help</a>
-                </Link>
-              </li>
-              <li>
-                <span></span>
-              </li>
-              <li>
-                <Link href='#'>
-                  <a>Facebook</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+  return (
+    <div className={styles.nav}>
+      <div className={styles.menu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    );
+      <div className={styles.horiMenu}>
+        <ul>
+          <li>
+            <Link href='#buy'>
+              <a>Buy</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='#blog'>
+              <a>Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='#help'>
+              <a>Help</a>
+            </Link>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <Link href='#'>
+              <a>Facebook</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
