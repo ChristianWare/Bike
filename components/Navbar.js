71d80d4d2af33cef60fdb04ff7c8e7c47a08bd95
Chris <chris.ware.dev@gmail.com>
2021-10-04 22:21:23 -0700

    Initial Setup/Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
+
+
+function Navbar() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.nav}>
+          <div className={styles.menu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={styles.horiMenu}>
+            <ul>
+              <li>
+                <Link href='#buy'>
+                  <a>Buy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='#blog'>
+                  <a>Blog</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='#help'>
+                  <a>Help</a>
+                </Link>
+              </li>
+              <li>
+                <span></span>
+              </li>
+              <li>
+                <Link href='#'>
+                  <a>Facebook</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Navbar
